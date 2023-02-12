20a2b20cf250a611dd1cf1ade360ca93afab0193
Chad Graham <mostgoose@gmail.com>
2023-02-11 18:27:11 -0800

    Campsites Directory Page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    hello nucamp!
-</div>
-   );
+    <div className='App'>
+      <Header />
+      <CampsitesDirectoryPage />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { store } from './app/store';
 import App from './App';
 
 import './index.css';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-social/bootstrap-social.css';
 import 'font-awesome/css/font-awesome.css';
@@ -25,4 +26,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
